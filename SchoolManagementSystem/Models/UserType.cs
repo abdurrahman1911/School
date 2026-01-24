12642717755512cba21592393edc6a97a71166e5
Abdurrahman Deghish <124841997+abdurrahman1911@users.noreply.github.com>
2026-01-24 20:48:10 +0200

    Update UserType model with new roles

--- SchoolManagementSystem/Models/UserType.cs
+++ SchoolManagementSystem/Models/UserType.cs
@@ -1,11 +1,17 @@
+using SchoolManagementSystem.ViewModel;
+
 namespace SchoolManagementSystem.Models
 {
     public class UserType
     {
-        public int ID { get; set; }
+        public Byte ID { get; set; }
         public string TypeName { get; set; }
 
         public virtual ICollection<User> Users { get; set; }
-    
+
+        public static explicit operator UserTypeEnum(UserType v)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
