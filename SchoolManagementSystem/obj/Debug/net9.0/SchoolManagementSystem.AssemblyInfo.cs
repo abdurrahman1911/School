a82513b4d6dea66bcbfe2feca7ec4ebb262346f5
Zeyad Ammar <zeyadammar2510@gmail.com>
2026-01-31 16:00:22 +0200

    fix a problem

--- SchoolManagementSystem/obj/Debug/net9.0/SchoolManagementSystem.AssemblyInfo.cs
+++ SchoolManagementSystem/obj/Debug/net9.0/SchoolManagementSystem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+860e8f8e163e8a4579ec179471c198e1ce161ec1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69aef40e5c3e457966b0e3949ef07c8d990b12b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
