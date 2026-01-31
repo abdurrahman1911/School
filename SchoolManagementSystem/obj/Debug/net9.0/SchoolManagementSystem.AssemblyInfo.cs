69aef40e5c3e457966b0e3949ef07c8d990b12b9
Zeyad Ammar <zeyadammar2510@gmail.com>
2026-01-31 15:49:20 +0200

    Pull the changes after ending my updates before make my push

--- SchoolManagementSystem/obj/Debug/net9.0/SchoolManagementSystem.AssemblyInfo.cs
+++ SchoolManagementSystem/obj/Debug/net9.0/SchoolManagementSystem.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8b09d3abf91fc8a79867f43d2f7de6dc1be5d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+860e8f8e163e8a4579ec179471c198e1ce161ec1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
