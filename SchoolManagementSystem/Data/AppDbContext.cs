6d907dbe0265c2b266b5123f854f1f2990ace212
Abdurrahman Deghish <124841997+abdurrahman1911@users.noreply.github.com>
2026-02-14 14:15:24 +0200

    first commit

--- SchoolManagementSystem/Data/AppDbContext.cs
+++ SchoolManagementSystem/Data/AppDbContext.cs
@@ -8,7 +8,7 @@ namespace SchoolManagementSystem.Data
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var connectionString = "Server=DESKTOP-H9CT4I0\\SQLEXPRESS;Database=SchoolManagementSystem;Integrated Security=SSPI;TrustServerCertificate=True";
+            var connectionString = "Server=.;Database=SchoolManagementSystem;Integrated Security=SSPI;TrustServerCertificate=True";
             optionsBuilder.UseSqlServer(connectionString, sqlOptions => 
             {
                 sqlOptions.EnableRetryOnFailure();
